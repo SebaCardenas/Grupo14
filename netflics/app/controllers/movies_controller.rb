5ee4a41410b5c6dfde696c978ffe9089bdf803bb
SebaCardenas <sacardenas@uc.cl>
2017-04-27 22:02:22 -0300

    create movies

--- netflics/app/controllers/movies_controller.rb
+++ netflics/app/controllers/movies_controller.rb
@@ -1,6 +1,26 @@
 class MoviesController < ApplicationController
-
   def index
+    @movies = Movie.all.order("create_at DESC")
   end
-  
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to root_path
+
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def movie_params
+      params.require(:movie).permit(:title, :description, :author)
+    end
 end
