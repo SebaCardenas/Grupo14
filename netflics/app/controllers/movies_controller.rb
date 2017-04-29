46fde5eae1ce7ad0edb3ba19be9d7745e224cae9
lukassr <lsvicarovic@uc.cl>
2017-04-29 19:56:40 -0300

    CanCan Implemented with user filters by guest and common

--- netflics/app/controllers/movies_controller.rb
+++ netflics/app/controllers/movies_controller.rb
@@ -1,50 +1,46 @@
 class MoviesController < ApplicationController
-
+  load_and_authorize_resource
 
   def index
-    if user_signed_in?
-      @movies = Movie.all
-    else
-      redirect_to "/login"
-    end
+      #@movies = Movie.all
 
   end
 
   def show
-    @movie = Movie.find(params[:id])
+    #@movie = Movie.find(params[:id])
   end
 
   def edit
-    @movie = Movie.find(params[:id])
+    #@movie = Movie.find(params[:id])
     render 'edit'
   end
 
   def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
-
     redirect_to movies_path
 
   end
 
   def new
-    @movie = Movie.new
+    #@movie = Movie.new
   end
 
   def create
-    @movie = Movie.new(movie_params)
+    #@movie.user_id = current_user.id
+    # @movie = Movie.new(movie_params)
+    #
+     if @movie.save
+       redirect_to @movie
 
-    if @movie.save
-      redirect_to @movie
-
-    else
-      render 'new'
-    end
+     else
+       render 'new'
+     end
   end
 
   private
 
-    def movie_params
-      params.require(:movie).permit(:title, :description, :director)
-    end
+     def movie_params
+       params.require(:movie).permit(:title, :description, :director)
+     end
 end
