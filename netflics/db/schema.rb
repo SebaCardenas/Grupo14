905a3439a0ee7797da9f216d8cca5e469a3a622e
SebaCardenas <sacardenas@uc.cl>
2017-04-29 18:28:36 -0300

    Add column Creator to Movies

--- netflics/db/schema.rb
+++ netflics/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170429195834) do
+ActiveRecord::Schema.define(version: 20170429212457) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170429195834) do
     t.datetime "date_update"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "creator"
   end
 
   create_table "users", force: :cascade do |t|
