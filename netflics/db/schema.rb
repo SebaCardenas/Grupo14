a112fe9a705cb0d308772c98921d0f021ba0f71b
lukassr <lsvicarovic@uc.cl>
2017-04-29 16:21:31 -0300

    Added name+lastname to user

--- netflics/db/schema.rb
+++ netflics/db/schema.rb
@@ -22,11 +22,14 @@ ActiveRecord::Schema.define(version: 20170429152031) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "lastname"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer  "role",                   default: 0,  null: false
     t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
