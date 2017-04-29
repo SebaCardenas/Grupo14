dcb30f8cf185881c32a84ccca3e8988dff68b762
Matus <fgmatus@uc.cl>
2017-04-29 18:53:21 -0300

    Country fix

--- netflics/app/controllers/movies_controller.rb
+++ netflics/app/controllers/movies_controller.rb
@@ -33,19 +33,21 @@ class MoviesController < ApplicationController
   def create
 
     @movie = Movie.new(movie_params)
-
     if @movie.save
       redirect_to @movie
 
     else
       render 'new'
     end
+    puts @movie.title
+    puts @movie.country
+    puts @movie.description
   end
 
   private
 
     def movie_params
-      params.require(:movie).permit(:title, :description, :category,
+      params.require(:movie).permit(:title, :description, :category, :country,
       :language, :actor, :director, :n_chapter, :n_season, :start_year,
       :finish_year, :date_update)
     end
