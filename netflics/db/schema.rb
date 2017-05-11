02e45893cb967f36eec0257777d3cebfe035b8c9
SebaCardenas <sacardenas@uc.cl>
2017-05-11 20:15:08 -0300

    Create Category Model

--- netflics/db/schema.rb
+++ netflics/db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170429221428) do
+ActiveRecord::Schema.define(version: 20170511231239) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
