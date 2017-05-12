5bd355abba461b1d8f184111d0b24840df66fc48
SebaCardenas <sacardenas@uc.cl>
2017-05-12 14:17:13 -0300

    Ready, Categories predefined and associated

--- netflics/db/schema.rb
+++ netflics/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170511231239) do
+ActiveRecord::Schema.define(version: 20170511232651) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 20170511231239) do
   create_table "movies", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.string   "category"
     t.string   "country"
     t.string   "language"
     t.text     "actor"
@@ -34,6 +33,7 @@ ActiveRecord::Schema.define(version: 20170511231239) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "creator"
+    t.integer  "category_id"
   end
 
   create_table "users", force: :cascade do |t|
