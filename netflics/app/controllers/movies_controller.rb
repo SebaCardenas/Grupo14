b94a8108cb4d9bc1cf787d34dc00e1eae4ed14cd
SebaCardenas <sacardenas@uc.cl>
2017-05-25 14:59:58 -0400

    filter by category Ready

--- netflics/app/controllers/movies_controller.rb
+++ netflics/app/controllers/movies_controller.rb
@@ -3,8 +3,12 @@ class MoviesController < ApplicationController
 
 
   def index
+    if params[:category].blank?
       #@movies = Movie.all
-
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+      @movies = Movie.where(:category_id => @category_id)
+    end
   end
 
   def show
