8a12d9f69fc4fb1a18ef1b2d8d8a16d98424da04
lukassr <lsvicarovic@uc.cl>
2017-06-24 18:37:44 -0400

    View Statistics per user

--- netflics/config/routes.rb
+++ netflics/config/routes.rb
@@ -8,10 +8,10 @@ Rails.application.routes.draw do
   devise_scope :user do
   root to: "movies#index"
   resources :articles
+  get "stats", to: "registrations#stats"
   end
   scope "/admin" do
   resources :users
-end
-
+  end
 
 end
